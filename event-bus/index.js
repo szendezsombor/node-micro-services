53244891282005793d2cedbbf0c4f230c81eb74f
Szende Zsombor (Alerant Zrt.) <Zsombor.Szende@ext.otpbank.hu>
2023-08-07 08:25:03 +0200

    finish the first microservice project

--- event-bus/index.js
+++ event-bus/index.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
+const events = [];
 
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
     const event = req.body;
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch(console.error);
     axios.post('http://localhost:4001/events', event).catch(console.error);
@@ -15,6 +17,10 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4003/events', event).catch(console.error);
 
     res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 })
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const {post} = require("axios");
+const axios = require("axios");
 
 const app = express();
 
@@ -9,12 +11,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
@@ -26,9 +23,39 @@ app.post('/events', (req, res) => {
         posts[postId].comments.push({ id, content, status });
     }
 
+    if(type === 'CommentUpdated') {
+        const { postId, id, status, content } = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment => comment.id === id);
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
+
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Application listening on port 4002');
+
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+        for (let event of res.data) {
+            console.log('processing event', event.type);
+            handleEvent(event.type, event.data);
+        }
+    } catch (e) {
+        console.error(e);
+    }
 })
