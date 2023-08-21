31b5fc5995ba1a83a8ea81785be3ef39c2999694
Szende Zsombor (Alerant Zrt.) <Zsombor.Szende@ext.otpbank.hu>
2023-08-21 16:37:04 +0200

    finish project

---
+++ client/build.sh
@@ -0,0 +1,2 @@
+docker build -t beregos/client .
+docker push beregos/client
--- infra/k8s/notes.sh
+++ infra/k8s/notes.sh
@@ -36,3 +36,7 @@ k logs posts-depl-6f7999bcd9-xnpvx
 
 # Pod container megnyitása
 k exec -it posts-depl-6f7999bcd9-xnpvx sh
+
+# Ingress NGINX
+# https://kubernetes.github.io/ingress-nginx/deploy/
+k apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
